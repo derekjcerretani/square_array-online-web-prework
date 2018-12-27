f1ea6bf825a709c0bc370e52c927c3ebc6c7a242
Derek Cerretani <derekcerretani@gmail.com>
2018-12-27 21:47:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,11 @@ def square_array(array)
   array.each {|x| arr << x ** 2}
   arr
 end
+
+def square_array(array)
+ sq_array = []
+ array.each do |num|
+ sq_array.push(num ** 2)
+ end
+ return sq_array
+end
